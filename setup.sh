da0fd31e85b291724c890d7e2d185964812adc77
mcossu <16074153+medaey@users.noreply.github.com>
2026-05-17 21:20:25 +0200

    Update rss2json installation script to use rss2json.py

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ pip install -r requirements.txt
 
 echo "Installing rss2json to /usr/local/bin..."
 
-sudo cp rss2json /usr/local/bin/rss2json
+sudo cp rss2json.py /usr/local/bin/rss2json
 sudo chmod +x /usr/local/bin/rss2json
 
 echo "Done. Try: rss2json https://example.com/rss.xml"
